f5d05f3e148dd7538d29fe33e6a7749ddecf59e1
Sharif Uddin <uddin15-4475@diu.edu.bd>
2022-08-02 20:06:13 +0600

    My first project by Java Progrram

---
+++ StandardAtm.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shari
+ */
+class StandardAtm {
+
+    void accessacc(String string, String string0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
